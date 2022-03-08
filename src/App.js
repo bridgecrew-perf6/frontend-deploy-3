5b28b379466ed94dae31dfbd64368bbb8c1b4467
Gleidson Braga <gleidiin@gmail.com>
2022-03-07 22:21:53 -0300

    chore(app): adicionando ci e arquivos iniciais

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="main">
+      <h1>Uma aplicação simples com React 🤩</h1>
+      <p>Utilizando Github Pages para subir essa aplicação linda</p>
+      <p>Sinta-se livre para pegar esse template e subir uma aplicação para você também!</p>
+    </div>
+  );
+}
+
+export default App;
